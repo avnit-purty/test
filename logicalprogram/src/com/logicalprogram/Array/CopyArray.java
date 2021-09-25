14c15b057b7174cb96d4d11ec9879e173c4f9916
avnit-purty <abanti.techie@gmail.com>
2021-09-25 16:20:26 +0530

    abanti changed

--- logicalprogram/src/com/logicalprogram/Array/CopyArray.java
+++ logicalprogram/src/com/logicalprogram/Array/CopyArray.java
@@ -1,10 +1,10 @@
 package com.logicalprogram.Array;
 
-import java.util.Arrays;
-
 public class CopyArray {
+	
 
 	public static void main(String[] args) {
+		
            int []arr=new int[] {1,2,3,4,5,6};
            int[]ar= new int[arr.length];
            for(int i=0;i<arr.length;i++) {
